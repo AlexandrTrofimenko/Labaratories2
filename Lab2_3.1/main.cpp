bff6d484f71c2b21f722be92fe25630eb2ec3287
Alexandr <ARTrofimenko@stud.kantiana.ru>
2022-02-28 22:44:29 +0200

    upd

---
+++ Lab2_3.1/main.cpp
@@ -0,0 +1,38 @@
+/*
+Вариант 6
+2.Найдите корень уравнения ln x – 1 = 0 с точностью до 10^-4 с помощью бисекции.
+*/
+#include <iostream>
+#include <cmath>
+double f(double a)
+{
+	return log(a)-1;
+}
+double bisection(double a,double b,double j)
+{
+	double fa = f(a);
+	while (true)
+	{
+		double x = (a + b) / 2;
+		if ((abs(x - a) < j) || (abs(x - b) < j))
+		{
+			return x;
+		}
+		else if (f(x) * fa > 0)
+		{
+			a = x;
+		}
+		else
+		{
+			b = x;
+		}
+	}
+}
+int main()
+{
+	std::cout << "ln (x)-1 = 0" << std::endl;
+	double eps = 1e-4;
+	double result = bisection(0, 10, eps);
+	std::cout << "x = " << result;
+	return 0;
+}
---
+++ Lab2_4/main.cpp
@@ -0,0 +1,108 @@
+/*
+Вариант 6.
+Создайте односвязный список из N = 10000 целых чисел и реализуйте операцию поиска в нем элемента k. 
+Выполните M = 1000 поисковых запросов.
+Реализуйте такую же операцию для массива. Сравните производительность
+*/
+#include <iostream>
+
+struct T_List
+{
+	T_List* next;
+	//std::string surname;
+	int age;
+};
+
+void ADD(T_List* head, int age)
+{
+	T_List* p = new T_List;
+	p->age = age;
+
+	p->next = head->next;
+	head->next = p;
+}
+
+void PRINT(T_List* head)
+{
+	T_List* p = head->next;
+	while (p != nullptr)
+	{
+		std::cout << p->age << std::endl;
+		p = p->next;
+	}
+}
+
+void DELETE(T_List* head)
+{
+	T_List* tmp;
+	T_List* p = head;
+	while (p->next != nullptr)
+	{
+		if (p->next->age % 2 == 0)
+		{
+			tmp = p->next;
+			p->next = p->next->next;
+			delete tmp;
+		}
+		else
+			p = p->next;
+	}
+}
+
+void CLEAR(T_List* head)
+{
+	T_List* tmp;
+	T_List* p = head->next;
+	while (p != nullptr)
+	{
+		tmp = p;
+		p = p->next;
+		delete tmp;
+	}
+}
+
+void DUPLICATE(T_List* head)
+{
+	T_List* p = head->next;
+	while (p != nullptr)
+	{
+		if (p->age % 2 == 1)
+		{
+			T_List* q = new T_List;
+			q->next = p->next;
+			p->next = q;
+			q->age = p->age;
+			p = p->next;
+		}
+		p = p->next;
+	}
+}
+
+void SWAPSORT(T_List* head)
+{
+	//for(int i=0;i<n-1;i++)
+	//	for(int j=i+1;j<n;j++)
+
+	T_List* p = head->next;
+	while (p->next->next != nullptr)
+	{
+		T_List* q = p->next;
+		while (q->next != nullptr)
+		{
+			if (p->age > q->age)
+				std::swap(p->age, q->age);
+			q = q->next;
+		}
+		p = p->next;
+	}
+}
+
+void FIND(T_List* head)
+{
+
+}
+int main()
+{
+
+	return 0;
+}
