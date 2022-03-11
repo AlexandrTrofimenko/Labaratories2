facf76d2cfa2c349d07d6d2dded931c33b7f981e
Alexandr <ARTrofimenko@stud.kantiana.ru>
2022-03-11 19:23:39 +0200

    upd

--- Lab2_4/main.cpp
+++ Lab2_4/main.cpp
@@ -5,50 +5,29 @@
 Реализуйте такую же операцию для массива. Сравните производительность
 */
 #include <iostream>
-
 struct T_List
 {
 	T_List* next;
-	//std::string surname;
-	int age;
+	int elements;
 };
 
-void ADD(T_List* head, int age)
+void ADD(T_List* head, int elements)
 {
 	T_List* p = new T_List;
-	p->age = age;
+	p->elements = elements;
 
 	p->next = head->next;
 	head->next = p;
 }
-
 void PRINT(T_List* head)
 {
 	T_List* p = head->next;
 	while (p != nullptr)
 	{
-		std::cout << p->age << std::endl;
+		std::cout << p->elements << std::endl;
 		p = p->next;
 	}
 }
-
-void DELETE(T_List* head)
-{
-	T_List* tmp;
-	T_List* p = head;
-	while (p->next != nullptr)
-	{
-		if (p->next->age % 2 == 0)
-		{
-			tmp = p->next;
-			p->next = p->next->next;
-			delete tmp;
-		}
-		else
-			p = p->next;
-	}
-}
-
 void CLEAR(T_List* head)
 {
 	T_List* tmp;
@@ -61,56 +40,46 @@ void CLEAR(T_List* head)
 	}
 }
 
-void DUPLICATE(T_List* head)
+bool FIND(T_List* head,int x)
 {
-	T_List* p = head->next;
-	while (p != nullptr)
-	{
-		if (p->age % 2 == 1)
-		{
-			T_List* q = new T_List;
-			q->next = p->next;
-			p->next = q;
-			q->age = p->age;
-			p = p->next;
-		}
-		p = p->next;
-	}
-}
-
-void SWAPSORT(T_List* head)
-{
-	//for(int i=0;i<n-1;i++)
-	//	for(int j=i+1;j<n;j++)
 	T_List* p = head->next;
 	while (p->next->next != nullptr)
 	{
-		T_List* q = p->next;
-		while (q->next != nullptr)
+		if (p->elements == x)
 		{
-			if (p->age > q->age)
-				std::swap(p->age, q->age);
-			q = q->next;
+			return true;
 		}
-		p = p->next;
+		else
+		{
+			p = p->next;
+		}
 	}
 }
-
 int main()
 {
+	setlocale(LC_ALL, "Rus");
 	srand(time(NULL));
 	T_List* head = new T_List;
 	head->next = nullptr;
 	int N = 10000;
 	int M = 1000;
-	int k = rand();
+//	int k = rand();
+	int x = 0;
 	for (int i = 0; i < N; i++)
 	{
-		ADD(head, rand() % 10);
+		ADD(head, rand() %10);
 	}
 //	PRINT(head);
+	std::cin >> x;
+	if (FIND(head,x) == true)
+	{
+		std::cout << x << "-найден" << std::endl;
+	}
+	else
+	{
+		std::cout << x << "-не найден" << std::endl;
+	}
 	CLEAR(head);
-	delete head;	
-
+	delete head;
 	return 0;
 }
